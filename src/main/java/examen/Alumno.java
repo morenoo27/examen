b8337e53047ab2c0fe485e5084b7a33ead4dc99d
moreno27 <alejanmm2000@gmail.com>
2021-05-06 13:16:15 +0200

    13:15

--- src/main/java/examen/Alumno.java
+++ src/main/java/examen/Alumno.java
@@ -13,8 +13,33 @@ import java.util.Map;
  */
 public class Alumno {
     
-    String nombre;
-    Map<String, String> calificaciones;
+    private String nombre;
+    private Map<String, String> calificaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Map<String, String> getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void setCalificaciones(Map<String, String> calificaciones) {
+        this.calificaciones = calificaciones;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    void setCalificaciones(Map<String, String> createMap, RegistroJSON registro) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
 }
--- src/main/java/examen/RegistrosToAlumnado.java
+++ src/main/java/examen/RegistrosToAlumnado.java
@@ -6,23 +6,96 @@
 package examen;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
  * @author alejandro
  */
 public class RegistrosToAlumnado {
-    
-    public static ArrayList<Alumno> registrosALista (ArrayList<RegistroJSON> registroJSON) {
-        
+
+    public static ArrayList<Alumno> registrosALista(ArrayList<RegistroJSON> registroJSON) {
+
         ArrayList<Alumno> listaAlumnos = new ArrayList<>();
-        
+
         for (RegistroJSON registro : registroJSON) {
-            
-            listaAlumnos.add(registro);
+
+            Alumno newAlumno = crearAlumno(registro);
+
+            listaAlumnos.add(newAlumno);
         }
-        
-        
-        return null;
+
+        return listaAlumnos;
+    }
+
+    /**
+     * metodo que crea un alumno a partir de un objeto tipo RegistroJSON
+     *
+     * @param registro registro que queremos convertir
+     * @return Objeto de tipo alumno
+     */
+    private static Alumno crearAlumno(RegistroJSON registro) {
+
+        Alumno alumno = new Alumno();
+
+        alumno.setNombre(registro.getAlumno());
+        alumno.setCalificaciones(createMap(obtenerIniciales(), registro));
+
+        return alumno;
+    }
+
+    /**
+     * metodo que , llamando al metodo de la clase LeerCSV obtiene las iniciales de cada modulo
+     * 
+     * @return Lista con las inicales de los modulos
+     */
+    private static ArrayList<String> obtenerIniciales() {
+
+        Map<String, String> asignaturas = LecturaCSV.leerArchivoCSV("nombresModulos", "./");
+        ArrayList<String> iniciales = new ArrayList<>();
+        for (Map.Entry<String, String> entry : asignaturas.entrySet()) {
+
+            iniciales.add(entry.getKey());
+        }
+        return iniciales;
+    }
+
+    /**
+     * metodo que crea una estructura Map con relaicon inicial->nota
+     *
+     * @param iniciales iniciales de los modulos
+     * @param registro registro en cuestion
+     * @return estructura Map con relaicon inicial->nota
+     */
+    private static Map<String, String> createMap(ArrayList<String> iniciales, RegistroJSON registro) {
+
+        Map<String, String> calificaiocnes = new TreeMap<>();
+
+        for (String inicial : iniciales) {
+
+            switch (inicial) {
+                case "EA":
+                    calificaiocnes.put(inicial, registro.getEa());
+                    break;
+                case "TII":
+                    calificaiocnes.put(inicial, registro.getTii());
+                    break;
+                case "TC":
+                    calificaiocnes.put(inicial, registro.getTc());
+                    break;
+                case "ING":
+                    calificaiocnes.put(inicial, registro.getIng());
+                    break;
+                case "FOL":
+                    calificaiocnes.put(inicial, registro.getFol());
+                    break;
+                case "CEAC":
+                    calificaiocnes.put(inicial, registro.getCeac());
+                    break;
+            }
+        }
+
+        return calificaiocnes;
     }
 }
